57b249dc2a788021d53ef8243ed44cb6ad6af3b2
Aleksey Borisov <aleksey.b@wecandevelopit.com>
2018-02-17 19:59:42 +0300

    login user

--- resources/assets/js/components/Game.vue
+++ resources/assets/js/components/Game.vue
@@ -19,36 +19,36 @@
     var GAME_IN_PROGRESS = 2;
     var GAME_DRAW = 3;
 
-    //верификация пользователя
-    var verifyUser = () => {
+    // //верификация пользователя
+    // var verifyUser = () => {
 
-        //Проверяем наличие акаунта
-        //Делаем запрос на получение акаутна по введенному логину
-        golosJs.api.getAccounts([username], function(err, result) {
-            //Проверяем есть ли возвращенные значения
-            if (result.length) {
-                //Берем публичный ключ
-                var pubWif = result[0].posting.key_auths[0][0];
-                //Проверяем введенный пользователем приватный ключ на валидность
-                if (golosJs.auth.isWif(privWif)) {
-                    //Проверяем соответствие приватного и публичного ключей
-                    if (golosJs.auth.wifIsValid(privWif, pubWif)) {
-                        //Привязываем данные Голоса к аккаунту пользователя
-                        console.log("Success!");
-                    } else {
-                        //Возвращаем сообщение об ошибке "Не соответствие приватного и публичного ключей"
-                        console.log('The private key does not match the public key');
-                    }
-                } else {
-                    //Возвращаем сообщение об ошибке "Указанный приватный ключ не является валидным"
-                    console.log('The private key specified is not valid');
-                }
-            } else {
-                //возврат ошибки "Учетная запись пользователя не определена"
-                console.log('user account is not defined')
-            }
-        });
-    };
+    //     //Проверяем наличие акаунта
+    //     //Делаем запрос на получение акаутна по введенному логину
+    //     golosJs.api.getAccounts([username], function(err, result) {
+    //         //Проверяем есть ли возвращенные значения
+    //         if (result.length) {
+    //             //Берем публичный ключ
+    //             var pubWif = result[0].posting.key_auths[0][0];
+    //             //Проверяем введенный пользователем приватный ключ на валидность
+    //             if (golosJs.auth.isWif(privWif)) {
+    //                 //Проверяем соответствие приватного и публичного ключей
+    //                 if (golosJs.auth.wifIsValid(privWif, pubWif)) {
+    //                     //Привязываем данные Голоса к аккаунту пользователя
+    //                     console.log("Success!");
+    //                 } else {
+    //                     //Возвращаем сообщение об ошибке "Не соответствие приватного и публичного ключей"
+    //                     console.log('The private key does not match the public key');
+    //                 }
+    //             } else {
+    //                 //Возвращаем сообщение об ошибке "Указанный приватный ключ не является валидным"
+    //                 console.log('The private key specified is not valid');
+    //             }
+    //         } else {
+    //             //возврат ошибки "Учетная запись пользователя не определена"
+    //             console.log('user account is not defined')
+    //         }
+    //     });
+    // };
 
     let addPost = (title, body) => {
         //Добавить пост
--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -4,43 +4,68 @@
             <img src="images/logo.png" alt="" class="start-img">
             <div class="input-group-item">
                 <input type="text" v-model="username" name="" placeholder="Имя пользователя">
-                <input type="text" v-model="masterPassword" name="" placeholder="Мастер пароль">
+                <input type="password" v-model="masterPassword" name="" placeholder="Мастер пароль">
             </div>
             <button type="button" class="submit-main-screen" v-on:click="login">Логин</button>
         </form>
     </div>
 </template>
 <script>
-    var golosJs = require('golos-js');
+    var golos = require('golos-js');
 
-    var verifyUser = (username, privWif) => {
+    var verifyUser = (username, password) => {
+
+        //проверяем наличие аккаунта
+        golos.api.getAccounts([username], function(err, result) {
+            //console.log('getAccounts', result);
+            let postingPubkey = result[0].posting.key_auths[0][0];
+            if (result.length) {
+                let roles = ['owner', 'active', 'posting', 'memo'];
+                let keys = golos.auth.getPrivateKeys(username, password, roles);
+                //console.log('postingPubkey', postingPubkey);
+                //console.log('keys', keys);
+                if (postingPubkey === keys.postingPubkey) {
+                    //console.log();
+                    let permissions =  {
+                        user: username,
+                        posting: keys.posting,
+                        active: keys.active
+                    }
+                    localStorage.permissions = JSON.stringify(permissions);
+                } else {
+                    console.log("Неверно имя пользователя либо пароль");
+                }
+            } else {
+                alert('В системе нет такого пользователя');
+            }
+        });
 
         //Проверяем наличие акаунта
         //Делаем запрос на получение акаутна по введенному логину
-        golosJs.api.getAccounts([username], function(err, result) {
-            //Проверяем есть ли возвращенные значения
-            if (result.length) {
-                //Берем публичный ключ
-                var pubWif = result[0].posting.key_auths[0][0];
-                //Проверяем введенный пользователем приватный ключ на валидность
-                if (golosJs.auth.isWif(privWif)) {
-                    //Проверяем соответствие приватного и публичного ключей
-                    if (golosJs.auth.wifIsValid(privWif, pubWif)) {
-                        //Привязываем данные Голоса к аккаунту пользователя
-                        console.log("Success!");
-                    } else {
-                        //Возвращаем сообщение об ошибке "Не соответствие приватного и публичного ключей"
-                        console.log('The private key does not match the public key');
-                    }
-                } else {
-                    //Возвращаем сообщение об ошибке "Указанный приватный ключ не является валидным"
-                    console.log('The private key specified is not valid');
-                }
-            } else {
-                //возврат ошибки "Учетная запись пользователя не определена"
-                console.log('user account is not defined')
-            }
-        });
+        // golosJs.api.getAccounts([username], function(err, result) {
+        //     //Проверяем есть ли возвращенные значения
+        //     if (result.length) {
+        //         //Берем публичный ключ
+        //         var pubWif = result[0].posting.key_auths[0][0];
+        //         //Проверяем введенный пользователем приватный ключ на валидность
+        //         if (golosJs.auth.isWif(privWif)) {
+        //             //Проверяем соответствие приватного и публичного ключей
+        //             if (golosJs.auth.wifIsValid(privWif, pubWif)) {
+        //                 //Привязываем данные Голоса к аккаунту пользователя
+        //                 console.log("Success!");
+        //             } else {
+        //                 //Возвращаем сообщение об ошибке "Не соответствие приватного и публичного ключей"
+        //                 console.log('The private key does not match the public key');
+        //             }
+        //         } else {
+        //             //Возвращаем сообщение об ошибке "Указанный приватный ключ не является валидным"
+        //             console.log('The private key specified is not valid');
+        //         }
+        //     } else {
+        //         //возврат ошибки "Учетная запись пользователя не определена"
+        //         console.log('user account is not defined')
+        //     }
+        // });
     };
 
     export default {
