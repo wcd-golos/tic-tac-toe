cde605ac1b9a63b7103334d78a23b94f143027de
Timur Zevriev <timur.z@wecandevelopit.com>
2018-02-18 11:23:56 +0300

    fix

--- resources/assets/js/store.js
+++ resources/assets/js/store.js
@@ -9,10 +9,10 @@ const store = new Vuex.Store({
         posting: '',
         active: '',
         state: 0,
-        win: false,
-        winclass: '',
-        game_golos_rate: 0.010, // колличество переводимых голосов
-        game_gbg_rate: 0.000, // колличество переводимых золотых голосов
+        win: false, // флаг выйгыша
+        winclass: '',  //класс стиля для перечеркивания доски в случае победы
+        game_golos_rate: 0.010, // количество переводимых голосов
+        game_gbg_rate: 0.000, // количество переводимых золотых голосов
         game_commision: 0.001, // доход гаранту/агенту
         agent: 'timur.zevriev', // гарант/агент
         aget_active_wif: '5JmAaq8NGKHEZDiccH1AtYfHPVpUo1TB2Ls89gACYpN3nchDPgG', // активный ключ агента
