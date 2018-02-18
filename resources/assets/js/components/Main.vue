2a26cb578bad4a3db3bc7c42395e11f5515cb40f
Timur Zevriev <timur.z@wecandevelopit.com>
2018-02-18 16:29:09 +0300

    fix

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -24,6 +24,7 @@
         created: function() {
             let permStorage = localStorage['permissions'];
             if (permStorage != undefined) {
+                console.log(permStorage);
                 let permissions = JSON.parse(permStorage);
                 //this.login = false;
                 //this.agreement = true;
@@ -51,7 +52,7 @@
             this.getCurrentState((state, game) => {
                 //this.agreement = state != STATUS_PLAYING;
                 //this.game = state == STATUS_PLAYING;
-                this.$store.commit('state', 1);
+//                this.$store.commit('state', 0);
                 //this.$store.commit('state', 2);
                 console.log(game);
                 this.$store.commit('game', game);
